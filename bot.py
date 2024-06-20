842a8f90f2b5912b30f2055611631c5270247a6d
SzymonWojturski <szykemcos137@gmail.com>
2024-06-20 22:20:54 +0200

    running

--- bot.py
+++ bot.py
@@ -1,3 +1,15 @@
 import sensitive_data as sd
+import discord
+from discord.ext import commands
 
-print(sd.SZYWOJ_ID)
+intents=discord.Intents.all()
+intents.message_content=True
+intents.members=True
+
+bot=commands.Bot(command_prefix="$",intents=intents,help_command=None)
+
+@bot.event
+async def on_ready():
+    print("zalogowany")
+
+bot.run(sd.TOKEN)
